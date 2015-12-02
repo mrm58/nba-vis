0a82f34447381043c1d3eafae6190cd1ef16d82e
Matthew Meister <matthew.meister@gmail.com>
2015-12-02 18:08:59 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+// gulpfile.js
+
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+
+var srcFiles = ['index.js', 'routes.js', 'routes/*.js'];
+
+gulp.task('myTask', function() {
+    console.log('Running my task!');
+});
+
+gulp.task('server', function() {
+    var server = gls('.');
+    server.start().then(function(result) {
+        console.log('Server exited with result:', result);
+    });
+    return gulp.watch(['index.js', 'models/*.js', 'routes.js', 'routes/*.js'], function(file) {
+      console.log(file);
+        server.start.apply(server);
+    });
+});
+
+var Karma = require('karma').Server;
+gulp.task('test:frontend', function(done) {
+    return new Karma({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done).start();
+});
+
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+
+gulp.task('test:backend:pre', function() {
+    return gulp.src(srcFiles)
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test:backend', ['test:backend:pre'], function() {
+    return gulp.src('test/back-end/**/*.js', { read: false })
+        .pipe(mocha())
+        .pipe(istanbul.writeReports());
+});
+
+gulp.task('watch:test:backend', function() {
+    process.env.NODE_ENV = "test";
+    gulp.run('test:backend');
+    return gulp.watch(srcFiles.concat(['test/back-end/**/*.js']), ['test:backend']);
+});
+
+gulp.task('watch:test:frontend', function(done) {
+    return new Karma({
+        configFile: __dirname + '/karma.conf.js'
+    }, done).start();
+});
+
+// var webFiles = ['js/**/*.js'];
+// var concat = require('gulp-concat');
+// var ngAnnotate = require('gulp-ng-annotate');
+// var uglify = require('gulp-uglify');
+// gulp.task('build', function() {
+//   return gulp.src(webFiles)
+//     .pipe(concat('tictactoe.js'))
+//     .pipe(ngAnnotate())
+//     .pipe(uglify())
+//     .pipe(gulp.dest('public/dist'));
+// });
+
+gulp.task('default', ['server']);
---
+++ index.js
@@ -0,0 +1,192 @@
+var express = require('express');
+//var app = express();
+var app = exports.app = express();
+
+// require('express-debug')(app, {});
+
+app.use('/bower_components',
+  express.static(__dirname + '/bower_components'));
+app.use('/public', express.static(__dirname + '/public'));
+
+app.set('port', process.env.PORT || 3000);
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());     // what is this?
+app.use(bodyParser.urlencoded( {extended: false} ));
+// var models = require('./models');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(cookieParser());
+// if (process.env.REDIS_URL) {
+//   var RedisStore = require('connect-redis')(session);
+
+//   app.use(session({
+//     store: new RedisStore({ url: process.env.REDIS_URL }),
+//     secret: 'I see undead people',
+//     saveUninitialized: false,
+//     resave: false
+//   }));
+// } else {
+//   var Sequelize = require('sequelize');
+//   var SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//   var sequelize = new Sequelize(
+//     "database",
+//     "username",
+//     "password", {
+//       "dialect": "sqlite",
+//       "storage": "./store/session.sqlite"
+//     });
+
+//   var store = new SequelizeStore({ db: sequelize });
+//   store.sync();
+//   app.use(session({
+//     saveUninitialized: false,
+//     resave: false,
+//     secret: 'I see dead people',
+//     store: store
+//   }));
+// }
+
+// app.use(require('flash')());
+
+// app.use(require('morgan')('dev'));    // what is this?
+
+app.set('view engine', 'jade');
+
+var nba = require('nba');
+var Promise = require('bluebird');
+var nbaAPI = Promise.promisifyAll(nba.api);
+var rp = require('request-promise');
+var moment = require('moment');
+var baseUrl = 'http://data.nba.com/data/';
+
+/*********************************************************************
+*--------------------------------------------------------------------*
+* NBA Functions                                                      *
+*--------------------------------------------------------------------*
+nba.api
+nba.findPlayer
+nba.playerIdFromName
+nba.players
+nba.ready
+nba.searchPlayers
+nba.sportVu
+nba.stats
+nba.teamIdFromName
+nba.teams
+nba.updatePlayers
+nba.updateTeams
+nba.usePromises
+*--------------------------------------------------------------------*
+* NBA API Functions                                                  *
+*   *Async versions are promisified by the Bluebird library          *
+*--------------------------------------------------------------------*
+nbaAPI.boxScoreAdvanced, nbaAPI.boxScoreAdvancedAsync
+nbaAPI.boxScoreFourFactors, nbaAPI.boxScoreFourFactorsAsync
+nbaAPI.boxScoreMisc, nbaAPI.boxScoreMiscAsync
+nbaAPI.boxScoreScoring, nbaAPI.boxScoreScoringAsync
+nbaAPI.boxScoreUsage, nbaAPI.boxScoreUsageAsync
+nbaAPI.commonTeamRoster, nbaAPI.commonTeamRosterAsync
+nbaAPI.playByPlay, nbaAPI.playByPlayAsync
+nbaAPI.playerDashPtReboundLogs, nbaAPI.playerDashPtReboundLogsAsync
+nbaAPI.playerDashPtShotLog, nbaAPI.playerDashPtShotLogAsync
+nbaAPI.playerInfo, nbaAPI.playerInfoAsync
+nbaAPI.playerProfile, nbaAPI.playerProfileAsync
+nbaAPI.playerSplits, nbaAPI.playerSplitsAsync
+nbaAPI.playersInfo, nbaAPI.playersInfoAsync
+nbaAPI.scoreboard, nbaAPI.scoreboardAsync
+nbaAPI.shots, nbaAPI.shotsAsync
+nbaAPI.teamHistoricalLeaders, nbaAPI.teamHistoricalLeadersAsync
+nbaAPI.teamInfoCommon, nbaAPI.teamInfoCommonAsync
+nbaAPI.teamPlayerDashboard, nbaAPI.teamPlayerDashboardAsync
+nbaAPI.teamSplits, nbaAPI.teamSplitsAsync
+nbaAPI.teamStats, nbaAPI.teamStatsAsync
+nbaAPI.teamYears, nbaAPI.teamYearsAsync
+*********************************************************************/
+
+/*********************************************************************
+rp('http://data.nba.com/data/json/nbacom/2015/gameline/20151129/games.json')
+  .then(function(jsonResp) {
+    todaysGames = jsonResp;
+  })
+  .catch(function(err) {
+    console.log('There was an error: ' + err);
+  });
+*********************************************************************/
+// app.use(require('./routes'));
+app.get('/', function(req, res) {
+  var todaysGames = {};
+  var today = new Date();
+  var yyyy = moment().format('YYYY');
+  var mm = moment().format('MM');
+  var season = (mm > 9) ? yyyy : yyyy - 1;
+  var todaysDate = moment().format('YYYYMMDD');
+  var dateUrl = baseUrl + 'json/nbacom/' + season + '/gameline/' + todaysDate + '/games.json';
+
+  rp(dateUrl)
+    .then(function(jsonResp) {
+      res.render('dateView', {games: JSON.parse(jsonResp).games, date: todaysDate, season: season});
+    })
+    .catch(function(err) {
+      res.send('There was an error');
+    });
+});
+
+// app.get('/', function(req, res) {
+//   var jsonObj = {nba: nba};
+//   var lebron = nba.findPlayer('lebron james');
+//   jsonObj.lebron = lebron;
+//   jsonObj.lebronID = lebron.playerId;
+
+//   nbaAPI.playerProfileAsync({playerId: lebron.playerId})
+//     .then(function(profile) {
+//       return nbaAPI.playerInfoAsync({playerId: lebron.playerId})
+//         .then(function(info) {
+//           jsonObj.lebronProfile = profile;
+//           jsonObj.lebronInfo = info;
+//           res.send(jsonObj);
+//         });
+//     }).catch(function(err){
+//       jsonObj.err = err;
+//       res.json(jsonObj);
+//     });
+// });
+
+// app.get('/team', function(req, res) {
+//   //nba.updateTeams();
+//   var jsonObj = {nba: nba};
+//   var knicksID = nba.teamIdFromName('knicks');
+//   nbaAPI.teamInfoCommonAsync({teamId: knicksID})
+//     .then(function(teamInfo) {
+//       return nbaAPI.commonTeamRosterAsync({teamId: knicksID})
+//         .then(function(teamRoster) {
+//           jsonObj.knicks = teamInfo;
+//           jsonObj.roster = teamRoster;
+//           res.json(jsonObj);
+//         });
+//     });
+// });
+
+
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+} //is used to make what comes back from Curl requests more user friendly
+
+if (process.env.NODE_ENV !== 'test') {
+  var server = exports.server = app.listen(app.get('port'), function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
+}
+
+// //var server = app.listen(3000, function() {
+// var server = app.listen(app.get('port'), function() {
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+// });
